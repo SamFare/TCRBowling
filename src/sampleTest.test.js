8d67c9d1b525feb84d0f323823c10a5a16ce127f
Samuel Fare <samuel.fare@gmail.com>
2019-01-22 09:43:08 +0000

    working

--- src/sampleTest.test.js
+++ src/sampleTest.test.js
@@ -7,5 +7,6 @@ test('score is 0 when user rolls 0', () => {
 
 test('when the game is full of single scores the final score is the sum of the scores', () => { 
   const x = new Array(20).fill(1)
+  x.forEach(rollValue => roll(rollValue))
 })
   
