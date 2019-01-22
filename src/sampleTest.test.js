08b0c7ea7033c781b2ba6e0f4027de5700a918a2
Samuel Fare <samuel.fare@gmail.com>
2019-01-22 21:11:02 +0000

    working

--- src/sampleTest.test.js
+++ src/sampleTest.test.js
@@ -6,8 +6,10 @@ test('score is 0 when user rolls 0', () => {
 })
 
 test('when the game is full of single scores the final score is the sum of the scores', () => { 
-  const x = new Array(20).fill(1)
-  x.forEach(rollValue => roll(rollValue))
+  const allRollsInGame = new Array(20).fill(1)
+  allRollsInGame.forEach(rollValue => roll(rollValue))
   expect(score()).toBe(20)
 })
-  
+
+test('When the player rolls a spare the game doubles the score of the next roll', () => { 
+})
