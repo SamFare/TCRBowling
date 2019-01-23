e7386656d9df5347f14a85ba4fc8b63e498db0c5
Samuel Fare <samuel.fare@gmail.com>
2019-01-23 22:43:49 +0000

    working

--- src/sampleTest.test.js
+++ src/sampleTest.test.js
@@ -7,7 +7,7 @@ test('score is 0 when user rolls 0', () => {
 
 test('when the game is full of single scores the final score is the sum of the scores', () => { 
   const allRollsInGame = new Array(10).fill({roll1: 1, roll2: 1})
-const gameScore = allRollsInGame.reduce((acc, frame) => roll(acc, frame), 0)
+  const gameScore = allRollsInGame.reduce((acc, frame) => roll(acc, frame), 0)
   expect(gameScore).toBe(20)
 })
 
