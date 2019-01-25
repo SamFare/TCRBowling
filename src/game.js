ea89d7009cfc3f8d2a8a718cbd7c84323344f01a
Samuel Fare <samuel.fare@gmail.com>
2019-01-25 22:28:53 +0000

    working

--- src/game.js
+++ src/game.js
@@ -2,7 +2,7 @@ const roll = (game, {roll1, roll2})  => {
   game.frames.push({roll1, roll2})
    return {
     totalScore: game.totalScore + roll1 + roll2, 
-    frames: game.frames 
+    frames: game.frames.slice() 
    }
 }
 
