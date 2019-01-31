3d5e12d8c9ada79d1e8f410aa3a9de4ccac553c1
Samuel Fare <samuel.fare@gmail.com>
2019-01-31 13:51:29 +0000

    working

--- src/game.js
+++ src/game.js
@@ -4,16 +4,16 @@ const roll = (game, {roll1, roll2})  => {
    const frames = game.frames.slice()
    frames.push({roll1, roll2})
 
+   let totalScore = game.totalScore;
+
    if(isSpare(game.frames[game.frames.length - 1 ])) { 
-    return {
-      totalScore: game.totalScore + (roll1 * 2) + roll2, 
-      frames 
-     }
+      totalScore += (roll1 * 2) + roll2;
+
+   } else {
+    totalScore += roll1 + roll2;
    }
-    return {
-      totalScore: game.totalScore + roll1 + roll2, 
-      frames 
-    }
+  
+   return { totalScore, frames }
 }
 
 module.exports = { roll }
