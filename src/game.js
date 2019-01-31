3bdac600f87e9ee9d359ffde88ab0dced787f000
Samuel Fare <samuel.fare@gmail.com>
2019-01-31 13:55:58 +0000

    working

--- src/game.js
+++ src/game.js
@@ -1,13 +1,14 @@
 const { isSpare } = require('./frame') 
 
 const calculateTotalScore = (game, {roll1, roll2}) => { 
-  if(isSpare(game.frames[game.frames.length - 1 ])) { 
+  if(isSpare(game.frames[game.frames.length - 1])) { 
     return game.totalScore + (roll1 * 2) + roll2;
 
   } else {
     return game.totalScore + roll1 + roll2;
   }
 }
+
 const roll = (game, rolls)  => { 
    const frames = game.frames.slice()
    frames.push(rolls)
