282ea9a67c77f4b268e9c9a43e6fdf278f90ed28
Samuel Fare <samuel.fare@gmail.com>
2019-01-31 13:55:23 +0000

    working

--- src/game.js
+++ src/game.js
@@ -1,19 +1,17 @@
 const { isSpare } = require('./frame') 
 
-const roll = (game, {roll1, roll2})  => { 
+const calculateTotalScore = (game, {roll1, roll2}) => { 
+  if(isSpare(game.frames[game.frames.length - 1 ])) { 
+    return game.totalScore + (roll1 * 2) + roll2;
+
+  } else {
+    return game.totalScore + roll1 + roll2;
+  }
+}
+const roll = (game, rolls)  => { 
    const frames = game.frames.slice()
-   frames.push({roll1, roll2})
-
-   let totalScore = game.totalScore;
-
-   if(isSpare(game.frames[game.frames.length - 1 ])) { 
-      totalScore += (roll1 * 2) + roll2;
-
-   } else {
-    totalScore += roll1 + roll2;
-   }
-  
-   return { totalScore, frames }
+   frames.push(rolls)
+   return { totalScore: calculateTotalScore(game, rolls), frames }
 }
 
 module.exports = { roll }
